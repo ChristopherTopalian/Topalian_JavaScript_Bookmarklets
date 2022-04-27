e15ce9bc57b0ec9d00bb8919cf5a2ff4c00fbda6
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 02:28:11 -0400

    Add files via upload

---
+++ jsbm_alert_simple_001.js
@@ -0,0 +1,6 @@
+javascript: alert("Howdy");
+
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_alert_simple_001.js */
+/* shows an alert message */
