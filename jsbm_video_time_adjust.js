c37e065949cce38100ec212b602ee436b407f94d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 18:01:13 -0400

    Add files via upload

---
+++ jsbm_video_time_adjust.js
@@ -0,0 +1,14 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+ /* jsbm_video_time_adjust.js */
+/* skip video back 2 seconds by using -2 */
+/* skip video forward 2 seconds by using 2 */
+function()
+{
+    function videoTimeAdjust(seconds)
+    {
+        let theTime = document.querySelector('video').currentTime += seconds;
+    }
+    videoTimeAdjust(2);
+}());
