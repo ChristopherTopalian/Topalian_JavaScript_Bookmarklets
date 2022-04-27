93888906708ee4b7577987136ffde814577550ba
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 18:01:36 -0400

    Update jsbm_video_time_adjust.js

--- jsbm_video_time_adjust.js
+++ jsbm_video_time_adjust.js
@@ -1,7 +1,7 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
- /* jsbm_video_time_adjust.js */
+/* jsbm_video_time_adjust.js */
 /* skip video back 2 seconds by using -2 */
 /* skip video forward 2 seconds by using 2 */
 function()
