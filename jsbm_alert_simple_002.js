dc65f1c56f38699de72e219d10bb9ca16fdff7ae
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 02:27:42 -0400

    Update and rename jsbm_alert_simple_001.js to jsbm_alert_simple_002.js

--- jsbm_alert_simple_002.js
+++ jsbm_alert_simple_002.js
@@ -1,7 +1,7 @@
 javascript:
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
-/* jsbm_alert_simple_001.js */
+/* jsbm_alert_simple_002.js */
 /* shows an alert message */
 
 alert("Howdy");
