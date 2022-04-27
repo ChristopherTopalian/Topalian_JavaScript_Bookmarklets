af5e53fc3c4e08b7551aa90a9325de2c915a31c9
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-27 12:19:36 -0400

    Add files via upload

---
+++ jsbm_yt_video_pause.js
@@ -0,0 +1,15 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_yt_video_pause.js */
+/* pause youtube video */
+function()
+{
+	function ytVideoPause()
+	{
+		let theVideo = document.getElementById("movie_player");
+
+		theVideo.pauseVideo();
+	}
+    ytVideoPause();
+}());
