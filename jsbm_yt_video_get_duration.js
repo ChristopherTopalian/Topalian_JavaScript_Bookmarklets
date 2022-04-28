303d7ad0839854337a426ad26b866d1f5e1061f7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-28 12:57:25 -0400

    Add files via upload

---
+++ jsbm_yt_video_get_duration.js
@@ -0,0 +1,17 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_yt_video_get_duration.js */
+/* shows youtube video duration time in seconds */
+function()
+{
+    function ytVideoGetDuration(theVideo)
+    {
+        theVideo = document.getElementById('movie_player');
+
+        let theDuration = theVideo.getDuration();
+
+        return theDuration + " seconds";
+    }
+    alert(ytVideoGetDuration());
+})();
