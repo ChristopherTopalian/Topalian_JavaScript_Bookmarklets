841e927c3e2c9d48de314202d339f14d5459d044
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-29 12:24:43 -0400

    Update jsbm_video_set_playback_rate_prompt.js

--- jsbm_video_set_playback_rate_prompt.js
+++ jsbm_video_set_playback_rate_prompt.js
@@ -2,7 +2,7 @@ javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
 /* jsbm_video_set_playback_rate_prompt.js */
-/* sets the video playback speed according to user input  */
+/* sets the video playback speed according to user input */
 function()
 {
     function videoSetPlaybackRatePrompt()
