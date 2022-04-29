b90362bddeffa0e4498b5a224503a7c25046f137
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-04-29 05:29:38 -0400

    Update jsbm_video_set_playback_rate.js

--- jsbm_video_set_playback_rate.js
+++ jsbm_video_set_playback_rate.js
@@ -2,7 +2,7 @@ javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
 /* jsbm_video_set_playback_rate.js */
-/* set the video playback speed */
+/* sets the video playback speed */
 function()
 {
     function videoSetPlaybackRate(speed)
