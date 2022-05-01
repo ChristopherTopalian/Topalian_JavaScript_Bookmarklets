817f1de5e038a9ef759e187f5bf005f144138568
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-01 06:13:46 -0400

    Add files via upload

---
+++ jsbm_class_person_name_full.js
@@ -0,0 +1,34 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_class_person_name_full.js */
+/* shows the full name of the person by using our Person class object */
+function()
+{
+    class Person
+    {
+        constructor(firstName, middleName, lastName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+         }
+
+        nameFull()
+        {
+            let fullName = this.firstName + " " +
+            this.middleName + " " +
+            this.lastName;
+
+            return fullName;
+         }
+    }
+
+    let player001 = new Person(
+        "Niobi",        /* firstName */
+        "Grace",      /* middleName */
+        "Amber"      /* lastName*/
+    );
+
+    alert(player001.nameFull());
+})();
