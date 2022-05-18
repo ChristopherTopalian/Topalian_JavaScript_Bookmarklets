91ee52aeea7309ef79925c7f993bbe48d2e67e83
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-17 22:13:47 -0400

    Update and rename jsbm_prompt_alert_greeting_class_003.js to jsbm_prompt_alert_greeting_class_002.js

--- class/jsbm_prompt_alert_greeting_class_002.js
+++ class/jsbm_prompt_alert_greeting_class_002.js
@@ -1,6 +1,6 @@
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
-/* jsbm_prompt_alert_greeting_class_003.js */
+/* jsbm_prompt_alert_greeting_class_002.js */
 /* gets input from the person */
 /* shows an alert message with a name greeting */
 
