1c706cfa5eb4d5f6b71a6435d7c0c0dc87f0670d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-20 20:44:30 -0400

    Add files via upload

---
+++ jsbm_prompt_alert_greeting_003_name_lowercase.js
@@ -0,0 +1,29 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_prompt_alert_greeting_003_name_lowercase.js */
+/* gets input from the person */
+/* determines which message to show, based on name */
+/* shows an alert message with a name greeting */
+
+function()
+{
+function nameInput(name)
+{
+    name = prompt("Enter Name");
+
+    let nameFormatted = name.toLowerCase();
+
+    if(nameFormatted == "tabitha")
+    {
+        return "Hi Tabitha.";
+    }
+    else
+    {
+        return "Hi " + name + "." + " Where is Tabitha?";
+    }
+}
+
+alert(nameInput());
+
+}());
