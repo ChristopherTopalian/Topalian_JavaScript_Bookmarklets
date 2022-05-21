7cb47c61e1f7558396c5cd082cceb39671da1eeb
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-20 21:53:04 -0400

    Add files via upload

---
+++ jsbm_prompt_alert_question_002_ask_until_correct.js
@@ -0,0 +1,40 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_prompt_alert_question_002_ask_until_correct.js */
+/* gets input from the person */
+/* determines which message to show, based on their answer */
+/* shows an alert message related to their answer */
+/* keeps asking the question until the person answers correctly */
+
+function()
+{
+    function questionInput(question)
+    {
+        let message;
+
+        do
+        {
+            question = prompt("What is 5 x 5?");
+
+            if(question < 25)
+            {
+                message = "Choose a higher value.";
+            }
+            else if(question > 25)
+            {
+                message = "Choose a lower value.";
+            }
+            else if(question == 25)
+            {
+                message = "Correct. \n 5 x 5 = 25";
+            }
+
+            alert(message);
+        }
+        while(question != 25);
+    }
+
+    questionInput();
+
+}());
