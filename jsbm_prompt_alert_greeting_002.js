c7ed3cc209267c47d8bc3f06e2e60e2e8a3dbf1a
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:30:39 -0400

    Update jsbm_prompt_alert_greeting_002.js

--- jsbm_prompt_alert_greeting_002.js
+++ jsbm_prompt_alert_greeting_002.js
@@ -1,5 +1,6 @@
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_prompt_alert_greeting_002.js */
 /* gets input from the person */
 /* shows an alert message with a name greeting */
