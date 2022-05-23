459d37e3abf7b1e002ca027066f03b666ac7873b
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:34:08 -0400

    Update jsbm_video_play.js

--- jsbm_video_play.js
+++ jsbm_video_play.js
@@ -1,8 +1,10 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_video_play.js */
 /* play video */
+    
 function()
 {
     function videoPlay()
@@ -11,5 +13,7 @@ function()
 
         theVideo.play();
     }
+    
     videoPlay();
+    
 }());
