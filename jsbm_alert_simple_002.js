4c5a859721e1eab82e7295be82ddc0e3973bfa87
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:22:25 -0400

    Update jsbm_alert_simple_002.js

--- jsbm_alert_simple_002.js
+++ jsbm_alert_simple_002.js
@@ -1,6 +1,7 @@
 javascript:
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_alert_simple_002.js */
 /* shows an alert message */
 
