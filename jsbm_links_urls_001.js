02bfe186978c9f86ae4f4ab0e016cc3515c78d54
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 12:40:46 -0400

    Add files via upload

---
+++ jsbm_links_urls_001.js
@@ -0,0 +1,44 @@
+javascript:(
+/* Dedicated to God the Father */
+/* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* jsbm_links_urls_001.js */
+/*
+gets all links on a page
+shows the href of each link using console.log and a textarea
+the textarea is located at the bottom of the page
+*/
+
+function()
+{
+    function linksGet()
+    {
+        let linksFormatted = "";
+
+        let theLinks = document.getElementsByTagName("a");
+
+        for (let x = 0; x < theLinks.length; x++)
+        {
+            linksFormatted += theLinks[x].href;
+
+            linksFormatted += "\n\n";
+        }
+
+        return linksFormatted;
+
+    }
+
+    function linksShow()
+    {
+        console.log(linksGet());
+
+        let theTextArea = document.createElement("textarea");
+        theTextArea.style.position = "absolute";
+        theTextArea.style.width = "700px";
+        theTextArea.style.height = "500px";
+        theTextArea.value = linksGet();
+        document.body.append(theTextArea);
+    }
+
+    linksShow();
+
+}());
