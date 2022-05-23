c1ee77cc63090df34b9266a95d96b98ba747476c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:32:23 -0400

    Update jsbm_url_open.js

--- jsbm_url_open.js
+++ jsbm_url_open.js
@@ -1,15 +1,19 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_url_open.js */
 /* opens the specified url in the current tab, or a new tab */
+
 function()
 {
     function urlOpen(theURL, option)
     {
          window.open(theURL, "_" + option);
     }
+    
     /* blank to open the url in a new tab */
     /* self to open the url in the current tab */
     urlOpen("https://www.google.com", "self");
+    
 }());
