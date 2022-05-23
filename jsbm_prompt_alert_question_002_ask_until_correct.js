3082323aee28c3b1267b7fd835201e9320dc3246
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:31:44 -0400

    Update jsbm_prompt_alert_question_002_ask_until_correct.js

--- jsbm_prompt_alert_question_002_ask_until_correct.js
+++ jsbm_prompt_alert_question_002_ask_until_correct.js
@@ -1,6 +1,7 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_prompt_alert_question_002_ask_until_correct.js */
 /* gets input from the person */
 /* determines which message to show, based on their answer */
