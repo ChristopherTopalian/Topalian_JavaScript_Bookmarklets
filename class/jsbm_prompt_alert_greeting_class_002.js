3a4f52a7f6fe230c2278663e1a0f513961843e4e
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:41:02 -0400

    Update jsbm_prompt_alert_greeting_class_002.js

--- class/jsbm_prompt_alert_greeting_class_002.js
+++ class/jsbm_prompt_alert_greeting_class_002.js
@@ -1,5 +1,6 @@
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_prompt_alert_greeting_class_002.js */
 /* gets input from the person */
 /* shows an alert message with a name greeting */
@@ -20,4 +21,5 @@ function()
     let greetingObject001 = new Greeting();
 
     alert(greetingObject001.nameInput("Hi", name));
+
 }());
