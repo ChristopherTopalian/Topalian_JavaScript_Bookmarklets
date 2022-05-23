62661f6df715459cb35d431bd346b9b4efd05518
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:31:04 -0400

    Update jsbm_prompt_alert_greeting_003_name_lowercase.js

--- jsbm_prompt_alert_greeting_003_name_lowercase.js
+++ jsbm_prompt_alert_greeting_003_name_lowercase.js
@@ -1,6 +1,7 @@
 javascript:(
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_prompt_alert_greeting_003_name_lowercase.js */
 /* gets input from the person */
 /* determines which message to show, based on name */
