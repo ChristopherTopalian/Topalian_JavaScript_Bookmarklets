309df4d296524c6272b5fae93a6970b07f0198a9
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-23 16:21:50 -0400

    Update jsbm_alert_simple_001.js

--- jsbm_alert_simple_001.js
+++ jsbm_alert_simple_001.js
@@ -2,5 +2,6 @@ javascript: alert("Howdy");
 
 /* Dedicated to God the Father */
 /* All Rights Reserved Christopher Topalian Copyright 2000-2022 */
+/* https://github.com/ChristopherTopalian */
 /* jsbm_alert_simple_001.js */
 /* shows an alert message */
