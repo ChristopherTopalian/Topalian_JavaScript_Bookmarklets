dcf695991ec91efad0a3a81886daacdb514f98cb
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-24 20:12:22 -0400

    Update jsbm_document_info_004.js

--- jsbm_document_info_004.js
+++ jsbm_document_info_004.js
@@ -63,10 +63,10 @@ function()
 
         infoDiv.addEventListener("contextmenu", function() { elementRemove("infoDivId"); });
 
-        let theBreak = document.createElement("div");
-        theBreak.innerHTML = "<br>";
-        document.body.append(theBreak);
-        theBreak.append(infoDiv);
+        let theBreakDiv = document.createElement("div");
+        theBreakDiv.innerHTML = "<br>";
+        document.body.append(theBreakDiv);
+        theBreakDiv.append(infoDiv);
     }
 
     documentInfoGetAndShow();
