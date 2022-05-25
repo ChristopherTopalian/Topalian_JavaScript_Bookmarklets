150c98f1ffeaaedeae949e3e4ee4e634164ed26c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-05-24 20:09:26 -0400

    Update jsbm_document_info_001.js

--- jsbm_document_info_001.js
+++ jsbm_document_info_001.js
@@ -40,10 +40,10 @@ function()
 
         infoDiv.innerHTML += '<a href = \'' + window.location.href + '\' target = "_blank"> ' + window.location.href + ' </a><br>';
 
-        let theBreak = document.createElement("div");
-        theBreak.innerHTML = "<br>";
-        document.body.append(theBreak);
-        theBreak.append(infoDiv);
+        let theBreakDiv = document.createElement("div");
+        theBreakDiv.innerHTML = "<br>";
+        document.body.append(theBreakDiv);
+        theBreakDiv.append(infoDiv);
     }
 
     documentInfoGetAndShow();
