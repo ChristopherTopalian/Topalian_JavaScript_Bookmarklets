abddc3331e8382a27a82c8c9b6bf67704c20493c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-02-11 02:12:21 -0500

    Add files via upload

---
+++ jsbm_document_designMode.js
@@ -0,0 +1,24 @@
+javascript:(
+
+/* Dedicated to God the Father */
+
+/* All Rights Reserved Christopher Topalian
+Copyright 2000-2023 */
+
+/* https://github.com/ChristopherTopalian */
+
+/* jsbm_document_designMode.js */
+
+/* allows the user to edit the webpage */
+
+function()
+{
+
+    function editPage()
+    {
+        document.designMode = "on";
+    }
+
+    editPage();
+
+}());
